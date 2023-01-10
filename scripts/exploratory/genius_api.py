6103f8bdac235cd2dac8071142de3a2ee922d084
qaboahene <boaheneadu20@gmail.com>
2023-01-10 19:56:08 +0000

    Update to code for best practices.

--- scripts/exploratory/genius_api.py
+++ scripts/exploratory/genius_api.py
@@ -2,32 +2,51 @@ import csv
 import requests
 
 class SongScraper:
-    def __init__(self, artist_name, song_name):
+    def __init__(self, artist_name:str, song_name:str,access_token:str):
         self.artist_name = artist_name
-        self.song_name = song_name
-        self.access_token = "YOUR_ACCESS_TOKEN"
+        self.song_name =song_name
+        self.access_token = access_token
         self.base_url = "https://api.genius.com"
         self.song_id = self.get_song_id()
         
-    def get_song_id(self):
+    def get_song_id(self) -> int:
+        """
+        Function to get the song id from the Genius API
+        """
         search_url = self.base_url + "/search"
         data = {'q': self.artist_name + " " + self.song_name}
-        headers = {'Authorization': "Bearer " + self.access_token}
+        headers = {'Authorization': f"Bearer {self.access_token}"}
         response = requests.get(search_url, params=data, headers=headers)
         json_response = response.json()
+        if response.status_code !=200:
+            raise ValueError(f"Failed to fetch song id: {json_response['meta']['message']}")
         return json_response['response']['hits'][0]['result']['id']
     
-    def get_lyrics(self):
-        song_url = self.base_url + "/songs/{}".format(self.song_id)
-        headers = {'Authorization': "Bearer " + self.access_token}
+    def get_lyrics(self)->str:
+        """
+        function to get the lyrics for the song
+        """
+        song_url = self.base_url + f"/songs/{self.song_id}"
+        headers = {'Authorization': f"Bearer {self.access_token}"}
         response = requests.get(song_url, headers=headers)
         json_response = response.json()
+        if response.status_code !=200:
+            raise ValueError(f"Failed to fetch lyrics for the song: {json_response['meta']['message']}")
         return json_response['response']['song']['lyrics']['text']
         
     def save_lyrics(self):
-        song_lyrics = self.get_lyrics()
-        with open('lyrics.csv', mode='w') as file:
-            writer = csv.writer(file)
-            writer.writerow(['artist_name', 'song_name', 'lyrics'])
-            writer.writerow([self.artist_name, self.song_name, song_lyrics])
+        """
+        function to save the lyrics to a csv file
+        """
+        try:
+            song_lyrics = self.get_lyrics()
+            with open('lyrics.csv', mode='w', newline='') as file:
+                fieldnames = ['artist_name', 'song_name', 'lyrics']
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow({'artist_name': self.artist_name, 'song_name': self.song_name,'lyrics': song_lyrics})
+            print(f"Lyrics saved to 'lyrics.csv' successfully")
+        except Exception as e:
+            print(f"Error Occured while saving the lyrics, {e}")
+
             
