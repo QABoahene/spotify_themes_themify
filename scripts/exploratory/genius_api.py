22b82f8f4d486cc7a805c92e167f3865f46463d0
qaboahene <boaheneadu20@gmail.com>
2023-01-20 19:16:40 +0000

    genius api code built

--- scripts/exploratory/genius_api.py
+++ scripts/exploratory/genius_api.py
@@ -1,56 +1,47 @@
-import csv
-import requests
+import pandas as pd
+import lyricsgenius
+from tqdm import tqdm
 
+# Temporary global variables
+access_token = "<YOUR_GENIUS_API_ACCESS_TOKEN>"
+csv_file = "sample_data.csv"
+artist_column = "artist"
+song_column = "song_title"
 
-class SongScraper:
-    def __init__(self, artist_name:str, song_name:str,access_token:str):
-        self.artist_name = artist_name
-        self.song_name =song_name
+class SongLyrics():
+    # constructor for the class
+    def __init__(self, access_token, csv_file):
         self.access_token = access_token
-        self.base_url = "https://api.genius.com"
-        self.song_id = self.get_song_id()
-        
-    def get_song_id(self) -> int:
-        """
-        Function to get the song id from the Genius API
-        """
-        search_url = self.base_url + "/search"
-        data = {'q': self.artist_name + " " + self.song_name}
-        headers = {'Authorization': f"Bearer {self.access_token}"}
-        response = requests.get(search_url, params=data, headers=headers)
-        json_response = response.json()
-        if response.status_code !=200:
-            raise ValueError(f"Failed to fetch song id: {json_response['meta']['message']}")
-        return json_response['response']['hits'][0]['result']['id']
-    
-    def get_lyrics(self)->str:
-        """
-        function to get the lyrics for the song
-        """
-        song_url = self.base_url + f"/songs/{self.song_id}"
-        headers = {'Authorization': f"Bearer {self.access_token}"}
-        response = requests.get(song_url, headers=headers)
-        json_response = response.json()
-        if response.status_code !=200:
-            raise ValueError(f"Failed to fetch lyrics for the song: {json_response['meta']['message']}")
-        return json_response['response']['song']['lyrics']['text']
-        
-    def save_lyrics(self):
-        """
-        function to save the lyrics to a csv file
-        """
-        try:
-            song_lyrics = self.get_lyrics()
-            with open('lyrics.csv', mode='w', newline='') as file:
-                fieldnames = ['artist_name', 'song_name', 'lyrics']
-                writer = csv.DictWriter(file, fieldnames=fieldnames)
-                writer.writeheader()
-                writer.writerow({'artist_name': self.artist_name, 'song_name': self.song_name,'lyrics': song_lyrics})
-            print(f"Lyrics saved to 'lyrics.csv' successfully")
-        except Exception as e:
-            print(f"Error Occured while saving the lyrics, {e}")
+        self.genius = lyricsgenius.Genius(access_token)
+        self.df = pd.read_csv(csv_file)
+        self.not_found = []
 
-            
-# Testing
-scraper = SongScraper("artist_name","song_name")
-scraper.save_lyrics()
+    # method to get lyrics and add them to the csv file
+    def get_lyrics(self, artist_column, song_column):
+        self.df['song_lyrics'] = None
+        for i, row in tqdm(self.df.iterrows(), total=len(self.df)):
+            try:
+                song = self.genius.search_song(row[song_column], row[artist_column])
+                lyrics =song.lyrics
+                self.df.at[i, 'song_lyrics'] = lyrics
+            except:
+                self.not_found.append(f'{row[artist_column]} - {row[song_column]}')
+                self.df.at[i, 'song_lyrics'] = None
+                continue
+        return self.df
+    
+    # method to generate report
+    def generate_report(self):
+        found = len(self.df) - len(self.not_found)
+        print(f'Lyrics found for {found} songs')
+        print(f'Lyrics not found for {len(self.not_found)} songs')
+        if self.not_found:
+            print("\nSongs for which lyrics were not found:")
+            for song in self.not_found:
+                print(song)
+
+
+#Test code
+song_lyrics = SongLyrics(access_token, csv_file)
+df = song_lyrics.get_lyrics(artist_column, song_column)
+song_lyrics.generate_report()
